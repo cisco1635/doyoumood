119b7856c6347336fd2797ae3ae25426c2d94a3c
BRASSELET Julien <julien.brasselet@soprasteria.com>
2018-12-19 13:13:33 +0100

    postVote complete

--- src/controller/vote.js
+++ src/controller/vote.js
@@ -1,41 +1,60 @@
 const fs = require('fs');
+const utf8 = require('utf8');
 
 /**
- * get all data from the file
+ * post a vote
  */
 module.exports.postVote = function (req, res) {
     var obj;
-    var today = Date.now();
-    var todayFormat =  [d.getFullYear(),pad(d.getMonth()+1),pad(d.getDate())].join('');
-    var path = '../data/' + todayFormat + '.data';
+    var d = new Date();
+    var todayFormat =  [d.getFullYear(),d.getMonth()+1,d.getDate()].join('');
+    var path = './data/' + todayFormat + '.data';
+    var template = './data/template.data';
     var mood = req.params.mood;
-    console.log(mood);
-	fs.readFile(path, 'utf8', function (err, data) {
-        obj = JSON.parse(data);
-        switch(mood) {
-            case "overjoyed":
-              obj.data.overjoyed = obj.data.overjoyed+1;
-              break;
-            case "happy":
-              obj.data.happy = obj.data.happy+1;
-              break;
-            case "neutral":
-              obj.data.neutral = obj.data.neutral+1;
-              break;
-            case "annoyed":
-              obj.data.annoyed = obj.data.annoyed+1;
-              break;
-            case "angry":
-              obj.data.angry = obj.data.angry+1;
-              break;
-            default:
-              break;
-          }
-    });
+
+    // delete file
+    if (fs.existsSync(path)) {
+        //fs.unlinkSync(path);
+    }
+    // use template for the day if not exists
+    var data;
+    if (!fs.existsSync(path)) {
+        data = fs.readFileSync(template,'utf8');
+    }
+    else {
+        data = fs.readFileSync(path,'utf8');
+    }
+
+    console.log("Parse file : " + data);
+    obj = JSON.parse(data);
+    console.log("before total:"+obj);
+    switch(mood) {
+        case "overjoyed":
+            obj.overjoyed = obj.overjoyed+1;
+            break;
+        case "happy":
+            obj.happy = obj.happy+1;
+            break;
+        case "neutral":
+            obj.neutral = obj.neutral+1;
+            break;
+        case "annoyed":
+            console.log("before:"+obj.annoyed);
+            obj.annoyed = obj.annoyed+1;
+            console.log("after:"+obj.annoyed);
+            break;
+        case "angry":
+            obj.angry = obj.angry+1;
+            break;
+        default:
+            break;
+        }
     
-    fs.writeFile(path, obj, function(err) {
+    console.log("after total:"+obj);
+    fs.writeFileSync(path, JSON.stringify(obj), 'utf8', function(err) {
         if (err) {
             console.log(err);
+            res.status(500).json(obj);
         }
     });
 
