74f21c212aaaf0419bd54bbc058dd4c405fd3411
BRASSELET Julien <julien.brasselet@soprasteria.com>
2019-01-05 15:21:42 +0100

    error for day and month <10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 
 import { VoteComponent } from './component/vote/vote.component';
 import { ReportComponent } from './component/report/report.component';
 
-const routes:Routes = [
+const routes: Routes = [
   {
     path: 'vote',
     component: VoteComponent
@@ -18,8 +25,8 @@ const routes:Routes = [
     component: ReportComponent
   },
   {
-    path: '',  
-    redirectTo: '/vote', 
+    path: '',
+    redirectTo: '/vote',
     pathMatch: 'full'
   }
 ]
@@ -32,8 +39,15 @@ const routes:Routes = [
   ],
   imports: [
     BrowserModule,
-	RouterModule.forRoot(routes),
-	HttpClientModule
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
