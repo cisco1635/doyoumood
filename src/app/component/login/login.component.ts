c05be20e5f45faae8f15990fe030c294ca1265d6
anthony.dasse <anthony.dasse.ext@orange.com>
2019-01-08 22:58:58 +0100

    EVOL|Authent

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenPayload } from '../../models/token-payload';
+import { AuthenticationService } from '../../service/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    credentials: TokenPayload = {
+      email: '',
+      password: ''
+    };
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+  
+    constructor(private auth: AuthenticationService, private router: Router) {}
+  
+    login() {
+      this.auth.login(this.credentials).subscribe(() => {
+        this.router.navigateByUrl('/vote');
+      }, (err) => {
+        console.error(err);
+      });
+    }
 
 }
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,98 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { User } from '../models/user';
+import { TokenPayload } from '../models/token-payload';
+import { TokenResponse } from '../models/token-response';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthenticationService {
 
-  constructor() { }
+  private token: string;
+  private serviceUrl =  "http://localhost:3000" ;
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  private saveToken(token: string): void {
+    localStorage.setItem('mean-token', token);
+    this.token = token;
+  }
+
+  private getToken(): string {
+    if (!this.token) {
+      this.token = localStorage.getItem('mean-token');
+    }
+    return this.token;
+  }
+
+  public logout(): void {
+    this.token = '';
+    window.localStorage.removeItem('mean-token');
+    this.router.navigateByUrl('/');
+  }
+
+  public getUserDetails(): User {
+    const token = this.getToken();
+    let payload;
+    if (token) {
+      payload = token.split('.')[1];
+      payload = window.atob(payload);
+      return JSON.parse(payload);
+    } else {
+      return null;
+    }
+  }
+
+  public isLoggedIn(): boolean {
+    const user = this.getUserDetails();
+    if (user) {
+      return user.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  }
+
+  private request(method: 'post'|'get', type: 'login'|'register'|'profile', user?: TokenPayload): any {
+    let base;
+  
+    if (method === 'post') {
+      return this.http.post(this.serviceUrl + `/api/${type}`, user).subscribe(
+        (data: TokenResponse) => {
+          if (data.token) {
+            this.saveToken(data.token);
+          }
+          return data;
+        }
+      );
+    } else {
+      return this.http.get(this.serviceUrl + `/api/${type}`, { headers: { Authorization: `Bearer ${this.getToken()}` }}).subscribe(
+        (data: TokenResponse) => {
+          if (data.token) {
+            this.saveToken(data.token);
+          }
+          return data;
+        }
+      );
+    }
+  }
+
+  public register(user: TokenPayload): any {
+    return this.request('post', 'register', user);
+  }
+  
+  public login(user: TokenPayload): any {
+    return this.request('post', 'login', user);
+  }
+  
+  public profile(): any {
+    return this.request('get', 'profile');
+  }
 }
