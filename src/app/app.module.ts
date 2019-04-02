9b497f6e6ede63ed0ac310135c41d9f3dc797486
BRASSELET Julien <julien.brasselet@soprasteria.com>
2019-04-02 21:00:23 +0200

    audit fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-
 import { VoteComponent } from './component/vote/vote.component';
 import { ReportComponent } from './component/report/report.component';
 import { LoginComponent } from './component/login/login.component';
@@ -15,13 +13,9 @@ import { ProfileComponent } from './component/profile/profile.component';
 import { UsersComponent } from './component/users/users.component';
 import { TeamsComponent } from './component/teams/teams.component';
 import { TeamComponent } from './component/team/team.component';
-
 import { AuthGuardService } from './service/auth-guard.service';
-
 import { MaterialModule } from './material.module';
 import { UserComponent } from './component/user/user.component';
-
-
 
 const routes: Routes = [
   {
@@ -64,7 +58,6 @@ const routes: Routes = [
     component: RegisterComponent
   }
 ]
-
 @NgModule({
   declarations: [
     AppComponent,
