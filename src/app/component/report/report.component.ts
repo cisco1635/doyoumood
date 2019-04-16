1f1828ccf9a022823e0903086826bb5189d202f0
BRASSELET Julien <julien.brasselet@soprasteria.com>
2019-04-16 20:01:24 +0200

    add comments in report

--- src/app/component/report/report.component.ts
+++ src/app/component/report/report.component.ts
@@ -56,7 +56,7 @@ export class ReportComponent implements OnInit {
     this.svc.getData(date1, date2).subscribe((data:Report)=>{
         
         this.nbVote = data.nbVote.toString();
-        //this.imgmoyenne = "assets/images/"+data.imgmoyenne+".png";
+        this.imgmoyenne = "assets/images/"+data.imgmoyenne+".png";
 
         // create donutChart
         this.donutChart = new Chart('donutCanvas', {
